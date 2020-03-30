853fc8405576070bc468344f9cd78e518b04cddf
Paul Kuruvilla <rohitpaulk@gmail.com>
2020-03-30 19:51:10 +0530

    Move asserts to test harness

--- tests/dockerfile_tester.rb
+++ tests/dockerfile_tester.rb
@@ -28,51 +28,23 @@ class DockerfileTester < TestHarness
     log_header("Testing #{course}-#{language_pack}")
 
     log_info "Building #{language} image without cache"
+    time_taken = assert_time_under(300) { build_image }
 
-    time_taken_without_cache = measure_time { build_image }
-
-    log_info "Took #{time_taken_without_cache} secs"
+    log_info "Took #{time_taken} secs"
     log_info ""
 
     log_info "Building #{language} image without cache"
+    assert_time_under(5) { build_image }
 
-    time_taken_with_cache = measure_time { build_image }
-
-    log_info "Took #{time_taken_with_cache} secs"
+    log_info "Took #{time_taken} secs"
     log_info ""
-
-    if time_taken_with_cache < 5
-      log_success "Measured time is under threshold of 5 seconds"
-      log_success ""
-    else
-      log_error "Measured time was above threshold of 5 seconds"
-      log_error ""
-      raise TestFailedError
-    end
   end
 
   def build_image
-    _, stdout_io, stderr_io, wait_thr = process = Open3.popen3(
-      "docker build -t #{slug} -f #{dockerfile_path} #{starter_dir}"
+    assert_stdout_contains(
+      "docker build -t #{slug} -f #{dockerfile_path} #{starter_dir}",
+      "Successfully tagged #{slug}"
     )
-    exit_code = wait_thr.value.to_i
-    stdout, stderr = stdout_io.read, stderr_io.read
-
-    unless exit_code == 0
-      log_error("Build exited with code #{exit_code}")
-      log_error(stdout) unless stdout.empty?
-      log_error(stderr) unless stderr.empty?
-      raise TestFailedError
-    end
-
-    unless stdout.include?("Successfully tagged #{slug}")
-      log_error("Expected 'Successfully tagged #{slug}' to be present.")
-
-      log_error("Build exited with code #{exit_code}")
-      log_error(stdout)
-      log_error(stderr)
-      raise TestFailedError
-    end
   end
 
   def slug
@@ -85,12 +57,5 @@ class DockerfileTester < TestHarness
 
   def starter_dir
     "compiled_starters/#{course}-starter-#{language}"
-  end
-
-  def measure_time
-    before = Time.now
-    yield
-    after = Time.now
-    after - before
   end
 end
--- tests/test_harness.rb
+++ tests/test_harness.rb
@@ -10,4 +10,40 @@ class TestHarness
       false
     end
   end
+
+  def assert_stdout_contains(command, expected_stdout)
+    _, stdout_io, stderr_io, wait_thr = process = Open3.popen3(command)
+    exit_code = wait_thr.value.to_i
+    stdout, stderr = stdout_io.read, stderr_io.read
+
+    unless exit_code == 0
+      log_error("Process exited with code #{exit_code}")
+      log_error(stdout) unless stdout.empty?
+      log_error(stderr) unless stderr.empty?
+      raise TestFailedError
+    end
+
+    unless stdout.include?(expected_stdout)
+      log_error("Expected '#{expected_stdout}' to be present.")
+      log_error("Output found: ")
+      log_error(stdout) unless stdout.empty?
+      log_error(stderr) unless stderr.empty?
+      raise TestFailedError
+    end
+  end
+
+  def assert_time_under(threshold_seconds)
+    before = Time.now
+    yield
+    after = Time.now
+    time_taken = after - before
+
+    if time_taken > threshold_seconds
+      log_error "Measured time was above #{threshold_seconds} seconds"
+      log_error ""
+      raise TestFailedError
+    end
+
+    time_taken.round(1)
+  end
 end
