441bbb79ec72d49eec8a8239d138d9b332af7890
Paul Kuruvilla <rohitpaulk@gmail.com>
2020-03-31 17:12:40 +0530

    Stream logs in real-time

--- tests/test_harness.rb
+++ tests/test_harness.rb
@@ -1,4 +1,40 @@
+require "stringio"
+
 class TestFailedError < StandardError
+end
+
+class MultiWriter
+  def initialize(*targets)
+    @targets = targets
+  end
+
+  def write(*args)
+    @targets.map { |t| t.write(*args) }.first
+  end
+
+  def close
+    @targets.each(&:close)
+  end
+end
+
+class PrefixedLineWriter
+  def initialize(prefix, target)
+    @prefix = prefix
+    @target = target
+    @last_char = "\n"
+  end
+
+  def write(msg)
+    bytes_written = 0
+    msg.to_s.chars.each do |char| 
+      @target.write(@prefix) if @last_char == "\n"
+      bytes_written += @target.write(char)
+      @last_char = char
+    end
+    bytes_written
+  end
+
+  def close; end
 end
 
 class TestHarness
@@ -12,9 +48,18 @@ class TestHarness
   end
 
   def assert_stdout_contains(command, expected_stdout, expected_exit_code=0)
-    _, stdout_io, stderr_io, wait_thr = process = Open3.popen3(command)
+    _, stdout_io, stderr_io, wait_thr = Open3.popen3(command)
+
+    puts ""
+
+    stdout_captured, stderr_captured = StringIO.new, StringIO.new
+    setup_io_relay(stdout_io, $stdout, stdout_captured)
+    setup_io_relay(stderr_io, $stderr, stderr_captured)
+
     exit_code = wait_thr.value.exitstatus
-    stdout, stderr = stdout_io.read, stderr_io.read
+    stdout, stderr = stdout_captured.string, stderr_captured.string
+
+    puts ""
 
     unless exit_code == expected_exit_code
       log_error("Process exited with code #{exit_code} (expected: #{expected_exit_code})")
@@ -33,6 +78,16 @@ class TestHarness
       log_plain_multiline(stderr) unless stderr.empty?
       raise TestFailedError
     end
+  end
+
+  def setup_io_relay(source, prefixed_destination, other_destination)
+    IO.copy_stream(
+      source,
+      MultiWriter.new(
+        PrefixedLineWriter.new("     ", prefixed_destination),
+        other_destination
+      )
+    )
   end
 
   def assert_time_under(threshold_seconds)
