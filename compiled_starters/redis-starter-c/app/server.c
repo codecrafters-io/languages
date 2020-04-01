536a0c8582ab6590f61eebc67a64a8370bfe7535
Paul Kuruvilla <rohitpaulk@gmail.com>
2020-04-01 12:25:31 +0530

    Add compiled starter

---
+++ compiled_starters/redis-starter-c/app/server.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+
+int main() {
+	// Disable output buffering
+	setbuf(stdout, NULL);
+
+	printf("Your code goes here!\n");
+
+	// Uncomment this block to pass the first stage
+	// 
+	// int server_fd, client_addr_len;
+	// struct sockaddr_in client_addr;
+	// 
+	// server_fd = socket(AF_INET, SOCK_STREAM, 0);
+	// if (server_fd == -1) {
+	// 	printf("Socket creation failed: %s...\n", strerror(errno));
+	// 	return 1;
+	// }
+	//
+	// // Since the tester restarts your program quite often, setting REUSE_PORT
+	// // ensures that we don't run into 'Address already in use' errors
+	// int reuse = 1;
+	// if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEPORT, &reuse, sizeof(reuse)) < 0) {
+	// 	printf("SO_REUSEPORT failed: %s \n", strerror(errno));
+	// 	return 1;
+	// }
+	// 
+	// struct sockaddr_in serv_addr = { .sin_family = AF_INET ,
+	// 								 .sin_port = htons(6379),
+	// 								 .sin_addr = { htonl(INADDR_ANY) },
+	// 								};
+	// 
+	// if (bind(server_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) != 0) {
+	// 	printf("Bind failed: %s \n", strerror(errno));
+	// 	return 1;
+	// }
+	// 
+	// int connection_backlog = 5;
+	// if (listen(server_fd, connection_backlog) != 0) {
+	// 	printf("Listen failed: %s \n", strerror(errno));
+	// 	return 1;
+	// }
+	// 
+	// printf("Waiting for a client to connect...\n"); 
+	// client_addr_len = sizeof(client_addr);
+	// 
+	// accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
+	// printf("Client connected\n"); 
+	// 
+	// close(server_fd);
+
+	return 0;
+}
