eeec3ef7821f9ab685b31425aaa02ed976b8dfcd
Paul Kuruvilla <rohitpaulk@gmail.com>
2020-04-01 12:08:50 +0530

    Add starter definition

--- template_compiler/definitions.rb
+++ template_compiler/definitions.rb
@@ -6,6 +6,7 @@ docker = Course.new(slug: "docker", name: "Docker")
 
 python = Language.new(slug: "python", name: "Python", repo_suffix: "python")
 go = Language.new(slug: "go", name: "Go", repo_suffix: "go")
+c = Language.new(slug: "c", name: "C", repo_suffix: "c")
 
 DEFINITIONS = [
   Repo.new(
@@ -34,6 +35,20 @@ DEFINITIONS = [
     template_attrs: {
       "required_executable": "go (1.13)",
       "user_editable_file": "app/server.go"
+    }
+  ),
+  Repo.new(
+    course: redis,
+    language: c,
+    file_mappings: [
+      FM.new("README.md", "redis/README.md"),
+      FM.new("codecrafters.yml", "codecrafters.yml"),
+      FM.new("app/server.c", "redis/c/app/server.c"),
+      FM.new("spawn_redis_server.sh", "redis/c/spawn_redis_server.sh", is_executable=true),
+    ],
+    template_attrs: {
+      "required_executable": "gcc",
+      "user_editable_file": "app/server.c"
     }
   )
 ]
