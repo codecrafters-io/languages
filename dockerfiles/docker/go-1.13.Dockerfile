2a230bea027ba535bf50f243256dea4f52720d6b
Paul Kuruvilla <rohitpaulk@gmail.com>
2020-04-03 14:32:56 +0530

    Fixup docker go starter

--- dockerfiles/docker/go-1.13.Dockerfile
+++ dockerfiles/docker/go-1.13.Dockerfile
@@ -2,8 +2,10 @@ FROM golang:1.13-alpine
 
 RUN apk add curl
 
+
 # Download docker-explorer
-RUN curl -Lo /usr/local/bin/docker-explorer https://github.com/codecrafters-io/docker-explorer/releases/download/${docker_explorer_version}/${docker_explorer_version}_linux_amd64
+ARG docker_explorer_version=v17
+RUN curl --fail -Lo /usr/local/bin/docker-explorer https://github.com/codecrafters-io/docker-explorer/releases/download/${docker_explorer_version}/${docker_explorer_version}_linux_amd64
 RUN chmod +x /usr/local/bin/docker-explorer
 
 COPY . /app
--- starter_templates/docker/go/app/main.go
+++ starter_templates/docker/go/app/main.go
@@ -6,20 +6,19 @@ import (
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 func main() {
-	fmt.Println("Implement your program here")
+	fmt.Println("Your code goes here!")
 
-	// Uncomment the code below to pass the first stage!
+	// Uncomment this block to pass the first stage!
 	//
 	// command := os.Args[3]
 	// args := os.Args[4:len(os.Args)]
-
+	//
 	// cmd := exec.Command(command, args...)
-
 	// output, err := cmd.Output()
 	// if err != nil {
 	// 	fmt.Printf("Err: %v", err)
 	// 	os.Exit(1)
 	// }
-
+	//
 	// fmt.Println(string(output))
 }
