f5da2df7e512bb077302f76c38ef30b071455d61
Scott MacCombie <scott.maccombie@gmail.com>
2020-05-02 21:02:39 -0400

    Use correct Print function to eliminate formatting warning

--- compiled_starters/git-starter-go/app/main.go
+++ compiled_starters/git-starter-go/app/main.go
@@ -14,12 +14,12 @@ func main() {
 	// case "init":
 	//     for _, dir := range []string{".git", ".git/objects", ".git/refs"} {
 	//         if err := os.Mkdir(dir, 0755); err != nil {
-	//             fmt.Println("Error creating directory: %s", err)
+	//             fmt.Printf("Error creating directory: %s\n", err)
 	//         }
 	//     }
 	//     headFileContents := []byte("ref: refs/heads/master\n")
 	//     if err := ioutil.WriteFile(".git/HEAD", headFileContents, 0644); err != nil {
-	// 	       fmt.Println("Error writing file: %s", err)
+	// 	       fmt.Printf("Error writing file: %s\n", err)
 	// 	   }
 	//     fmt.Println("Initialized git directory")
 	//
