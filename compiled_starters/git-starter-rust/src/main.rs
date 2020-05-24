98224cce5f829d8fa52812f08ab5b22375aee608
Paul Kuruvilla <rohitpaulk@gmail.com>
2020-05-24 23:21:57 +0530

    Fix compilation

--- compiled_starters/git-starter-rust/src/main.rs
+++ compiled_starters/git-starter-rust/src/main.rs
@@ -8,11 +8,11 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     if args[1] == "init" {
         // Uncomment this block to pass the first stage
-        fs::create_dir(".git").unwrap();
-        fs::create_dir(".git/objects").unwrap();
-        fs::create_dir(".git/refs").unwrap();
-        fs::write(".git/HEAD", "ref: refs/heads/master\n").unwrap();
-        println!("Initialized git directory")
+        // fs::create_dir(".git").unwrap();
+        // fs::create_dir(".git/objects").unwrap();
+        // fs::create_dir(".git/refs").unwrap();
+        // fs::write(".git/HEAD", "ref: refs/heads/master\n").unwrap();
+        // println!("Initialized git directory")
     } else {
         println!("unknown command: {}", args[1])
     }
