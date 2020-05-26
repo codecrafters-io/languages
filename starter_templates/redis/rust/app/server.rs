46e0b9038f0de61fdbe15a6f8dd28b980113f5de
Syed Ghufran Hassan <syedghufranhassan@gmail.com>
2020-05-26 21:08:40 +0500

    Add files via upload

--- starter_templates/redis/rust/app/server.rs
+++ starter_templates/redis/rust/app/server.rs
@@ -1,1 +1,15 @@
 
+
+use std::net::TcpListener;
+
+fn main() {
+
+    println!("Your code goes here!")
+
+     // Uncomment the below lines to pass the first stage
+  
+      //TcpListener::bind("127.0.0.1:6379").unwrap(); 
+  
+     // println!("Connection established"); 
+  }
+  
