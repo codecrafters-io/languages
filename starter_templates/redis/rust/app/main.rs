40b8d0fa0a02597f287fdb0dee89f9c82a3d6322
Syed Ghufran Hassan <syedghufranhassan@gmail.com>
2020-05-27 13:39:24 +0500

    Update main.rs

--- starter_templates/redis/rust/app/main.rs
+++ starter_templates/redis/rust/app/main.rs
@@ -6,7 +6,7 @@ fn main() {
 
     println!("Your code goes here!")
 
-     // Uncomment the below lines to pass the first stage
+     // Uncomment the below lines to pass first stage
   
       //TcpListener::bind("127.0.0.1:6379").unwrap(); 
   
