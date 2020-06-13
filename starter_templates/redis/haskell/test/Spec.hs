1c88c8ec0c90b315ca17bd961206d5a37b70efc3
Sherub Thakur <sherub.thakur@gmail.com>
2020-06-13 16:46:55 +0530

    Add starter templates for redis haskell

---
+++ starter_templates/redis/haskell/test/Spec.hs
@@ -0,0 +1,7 @@
+main :: IO ()
+main = do
+    putStrLn "Test suite not yet implemented"
+
+    -- HACK: Fix uncomment-er to work on specific paths?
+    -- Uncomment this 
+    -- -- Still a comment
