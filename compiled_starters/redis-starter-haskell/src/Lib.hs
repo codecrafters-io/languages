2f665fac68110acf578e56e7223666ecef101943
Sherub Thakur <sherub.thakur@gmail.com>
2020-06-13 19:10:05 +0530

    Add redis haskell compiled

---
+++ compiled_starters/redis-starter-haskell/src/Lib.hs
@@ -0,0 +1,10 @@
+module Lib
+    ( someFunc
+    ) where
+
+-- HACK: Fix uncomment-er to work on specific paths?
+-- Uncomment this 
+-- -- Still a comment
+
+someFunc :: IO ()
+someFunc = putStrLn "someFunc"
