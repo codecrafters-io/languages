72eb97f1c6eebc28c07a5ef2177585c784b24023
null n/a <testUser0x00AF@gmail.com>
2020-06-28 10:02:11 -0400

    Git: Kotlin support signal forwarding
    As pointed out by rohitpaulk, we need to forward signals to the running
    process for proper cleanup and to ensure timeouts are adhered to. The
    simplest way seems to be his rec to use exec, which replaces the shell
    of the script with the child process.

--- starter_templates/git/kotlin/your_git.sh
+++ starter_templates/git/kotlin/your_git.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 set -e
 
-tmpFile=$(mktemp tmp.XXXXXXXXXX.jar)
+tmpFile=$(mktemp --suffix=.jar)
 kotlinc "$(dirname $0)/app/main.kt" -include-runtime -d "$tmpFile"
-kotlin -classpath "$tmpFile" MainKt "$@"
-
-rm "$tmpFile"
+exec kotlin -classpath "$tmpFile" MainKt "$@"
