7fbb6caa36b21e3f909d49a192dbcca3d64149e7
Paul Kuruvilla <rohitpaulk@gmail.com>
2020-07-19 19:11:29 +0530

    Keep sample file barebones

--- starter_templates/redis/elixir/lib/server.ex
+++ starter_templates/redis/elixir/lib/server.ex
@@ -1,13 +1,13 @@
 defmodule Server do
   @moduledoc """
-  Module containing implementation for the redis server
+  Your implementation of a Redis server
   """
 
   use Application
 
   def start(_type, _args) do
     children = [
-      Server.accept()
+      Server.listen()
     ]
 
     opts = [strategy: :simple_one_for_one, name: Server.Supervisor]
@@ -15,26 +15,14 @@ defmodule Server do
   end
 
   @doc """
-  Function to start the server
+  Listen for incoming connections
   """
-  def accept() do
+  def listen() do
     IO.puts("Your code goes here!")
-    socket = "dummy_val_remove_this_line"
+
     # Uncomment this block to pass the first stage
     #
     # {:ok, socket} = :gen_tcp.listen(6379, [:binary, active: false, reuseaddr: true])
-    loop_acceptor(socket)
-  end
-
-  @doc """
-  Loop to ensure the server can execute. 
-  """
-  def loop_acceptor(socket) do
-    # Uncomment this block to pass the first stage
-    #
     # {:ok, _client} = :gen_tcp.accept(socket)
-    # :gen_tcp.recv(socket, 0)
-    # Process.exit(self(), :kill) # To kill the process. Remove this line once you can read messages from sockets.
-    loop_acceptor(socket)
   end
 end
