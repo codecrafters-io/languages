4ec17b14d638688485c0dbd01a04abeca67fd5cf
Paul Kuruvilla <rohitpaulk@gmail.com>
2020-08-11 22:28:08 +0530

    Add warnings against editing .sh files

--- starter_templates/git/ruby/your_git.sh
+++ starter_templates/git/ruby/your_git.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
+#
+# DON'T EDIT THIS!
+#
+# CodeCrafters uses this file to test your code. Don't make any changes here!
+#
+# DON'T EDIT THIS!
 exec ruby $(dirname $0)/app/main.rb "$@"
--- starter_templates/redis/elixir/spawn_redis_server.sh
+++ starter_templates/redis/elixir/spawn_redis_server.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
+#
+# DON'T EDIT THIS!
+#
+# CodeCrafters uses this file to test your code. Don't make any changes here!
+#
+# DON'T EDIT THIS!
 set -e
 exec mix run --no-halt
