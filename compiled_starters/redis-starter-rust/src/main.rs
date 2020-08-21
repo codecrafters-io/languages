cbfade820ffc7114152242a602d5a6c86d2dbeb0
Paul Kuruvilla <rohitpaulk@gmail.com>
2020-08-21 14:47:25 +0530

    fix compiled templates

--- compiled_starters/redis-starter-rust/src/main.rs
+++ compiled_starters/redis-starter-rust/src/main.rs
@@ -9,9 +9,9 @@ fn main() {
     println!("Your code goes here!");
 
     // Uncomment this block to pass the first stage
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    match listener.accept() {
-        Ok((_socket, addr)) => println!("accepted new client: {:?}", addr),
-        Err(e) => println!("couldn't accept client: {:?}", e),
-    }
+    // let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    // match listener.accept() {
+    //     Ok((_socket, addr)) => println!("accepted new client: {:?}", addr),
+    //     Err(e) => println!("couldn't accept client: {:?}", e),
+    // }
 }
