bec4c28132d3b2f5bc4b9cd662d061dbbf3e7fd9
FastestCoderOnEarth <jonathandalgaard01@icloud.com>
2020-12-21 17:38:19 +0100

    Add support for PHP in the docker challenge.

---
+++ starter_templates/docker/php/Dockerfile
@@ -0,0 +1,16 @@
+FROM php:7.4-cli-alpine
+
+RUN apk add --update-cache --upgrade curl
+RUN apk add --update-cache --upgrade curl-dev
+
+RUN docker-php-ext-install pcntl
+
+# Download docker-explorer.
+ARG docker_explorer_version=v18
+RUN curl --fail -Lo /usr/local/bin/docker-explorer https://github.com/codecrafters-io/docker-explorer/releases/download/${docker_explorer_version}/${docker_explorer_version}_linux_amd64
+RUN chmod +x /usr/local/bin/docker-explorer
+
+COPY . /app
+WORKDIR /app
+
+ENTRYPOINT ["/app/your_docker.sh"]
---
+++ starter_templates/docker/php/app/main.php
@@ -0,0 +1,25 @@
+<?php
+// Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
+
+// Disable output buffering.
+while (ob_get_level() !== 0) {
+  ob_end_clean();
+}
+
+echo "Implement your program here!";
+
+/* Comment out the code below to pass the first stage */
+// $command = $argv[3];
+// $child_pid = pcntl_fork();
+// if ($child_pid == -1) {
+//   echo "Error forking!";
+// }
+// elseif ($child_pid) {
+//   // We're in parent.
+//   pcntl_wait($status);
+//   echo "Child terminates!";
+// }
+// else {
+//   // Replace current program with calling program.
+//   exec(implode(' ', array_slice($argv, 3)));
+// }
