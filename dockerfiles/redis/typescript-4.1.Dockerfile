03770e58ec07178e67ad8934f164b13652c77ed3
FastestCoderOnEarth <jonathandalgaard01@icloud.com>
2021-01-04 18:03:11 +0100

    Test

--- dockerfiles/redis/typescript-4.1.Dockerfile
+++ dockerfiles/redis/typescript-4.1.Dockerfile
@@ -1,4 +1,7 @@
 FROM mhart/alpine-node:14
 
+RUN npm install
+RUN ./node_modules/.bin/tsc --build tsconfig.json
+
 COPY . /app
 WORKDIR /app
--- starter_templates/redis/typescript/spawn_redis_server.sh
+++ starter_templates/redis/typescript/spawn_redis_server.sh
@@ -5,6 +5,4 @@
 # CodeCrafters uses this file to test your code. Don't make any changes here!
 #
 # DON'T EDIT THIS!
-npm install
-./node_modules/.bin/tsc --build tsconfig.json
 exec node app/main.js
