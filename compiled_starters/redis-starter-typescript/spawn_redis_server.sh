6c2909a426efc7979825d1a5aa2a85c1b1b9aa76
FastestCoderOnEarth <jonathandalgaard01@icloud.com>
2021-01-04 18:16:55 +0100

    Test

--- compiled_starters/redis-starter-typescript/spawn_redis_server.sh
+++ compiled_starters/redis-starter-typescript/spawn_redis_server.sh
@@ -5,5 +5,6 @@
 # CodeCrafters uses this file to test your code. Don't make any changes here!
 #
 # DON'T EDIT THIS!
-./node_modules/.bin/tsc --build tsconfig.json
+node_modules/.bin/tsc --build tsconfig.json
+tsc --build tsconfig.json
 exec node app/main.js
