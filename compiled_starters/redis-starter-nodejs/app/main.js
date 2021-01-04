9fd7eb1d178020578ed90e5fd72cc5e3c75aaed4
FastestCoderOnEarth <jonathandalgaard01@icloud.com>
2021-01-04 16:04:39 +0100

    Add compiled version too.

--- compiled_starters/redis-starter-nodejs/app/main.js
+++ compiled_starters/redis-starter-nodejs/app/main.js
@@ -1,11 +1,18 @@
 const net = require('net');
 
-console.log("Implement your program here!");
+console.log("Your code goes here!");
 
 // Uncomment this block to pass the first stage
-// const server = net.createServer(function(socket) {
-//   socket.write('Echo server\r\n');
+// const server = net.createServer(socket => {
 //   socket.pipe(socket);
 // });
 //
 // server.listen(6379, '127.0.0.1');
+//
+// server.on('request', request => {
+//   const connection = request.accept(null, request.origin);
+//
+//   connection.on('close',() => {
+//     console.log('Client has disconnected.');
+//   });
+// });
--- starter_templates/redis/nodejs/app/main.js
+++ starter_templates/redis/nodejs/app/main.js
@@ -3,17 +3,16 @@ const net = require('net');
 console.log("Your code goes here!");
 
 // Uncomment this block to pass the first stage
-// const server = net.createServer(function(socket) {
-//   socket.write('Echo server\r\n');
+// const server = net.createServer(socket => {
 //   socket.pipe(socket);
 // });
 //
 // server.listen(6379, '127.0.0.1');
 //
-// server.on('request', function(request) {
+// server.on('request', request => {
 //   const connection = request.accept(null, request.origin);
 //
-//   connection.on('close', function() {
+//   connection.on('close',() => {
 //     console.log('Client has disconnected.');
 //   });
 // });
