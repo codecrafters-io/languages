daacbe3ec7a3de391e66538b600f71a0e09c425c
FastestCoderOnEarth <jonathandalgaard01@icloud.com>
2021-01-04 15:54:29 +0100

    Test

--- starter_templates/redis/nodejs/app/main.js
+++ starter_templates/redis/nodejs/app/main.js
@@ -1,6 +1,6 @@
 const net = require('net');
 
-console.log("Implement your program here!");
+console.log("Your code goes here!");
 
 // Uncomment this block to pass the first stage
 // const server = net.createServer(function(socket) {
@@ -9,3 +9,11 @@ console.log("Implement your program here!");
 // });
 //
 // server.listen(6379, '127.0.0.1');
+//
+// server.on('request', function(request) {
+//   const connection = request.accept(null, request.origin);
+//
+//   connection.on('close', function() {
+//     console.log('Client has disconnected.');
+//   });
+// });
