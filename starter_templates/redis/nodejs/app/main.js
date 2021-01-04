fe21c1c469c5502ce21906380e5fb88fc19e2abe
FastestCoderOnEarth <jonathandalgaard01@icloud.com>
2021-01-04 13:57:00 +0100

    Add support for JS in the redis challenge.

---
+++ starter_templates/redis/nodejs/app/main.js
@@ -0,0 +1,11 @@
+const net = require('net');
+
+console.log("Implement your program here!");
+
+/* Comment out the code below to pass the first stage */
+// const server = net.createServer(function(socket) {
+//   socket.write('Echo server\r\n');
+//   socket.pipe(socket);
+// });
+//
+// server.listen(6379, 'localhost');
