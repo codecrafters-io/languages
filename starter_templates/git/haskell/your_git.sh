72ba5df59e6e8d9530ce226721badf37e51193eb
Paul Kuruvilla <rohitpaulk@gmail.com>
2021-02-21 23:41:05 +0530

    specify STACK_YAML

--- starter_templates/git/haskell/your_git.sh
+++ starter_templates/git/haskell/your_git.sh
@@ -5,4 +5,4 @@
 # CodeCrafters uses this file to test your code. Don't make any changes here!
 #
 # DON'T EDIT THIS!
-exec stack run --silent "$@"
+STACK_YAML=$(dirname "$0")/stack.yaml exec stack run --silent "$@"
