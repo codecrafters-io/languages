61860e1b55b9fca85d4f25941c31a077d2462942
Paul Kuruvilla <rohitpaulk@gmail.com>
2021-07-24 19:27:02 +0100

    Add crystal langpack

--- compiled_starters/redis-starter-crystal/app/main.cr
+++ compiled_starters/redis-starter-crystal/app/main.cr
@@ -5,8 +5,8 @@ class YourRedisServer
     puts("Your code goes here!")
 
     # Uncomment this block to pass the first stage
-    server = TCPServer.new("localhost", 6379, reuse_port: true)
-    client = server.accept?
+    # server = TCPServer.new("localhost", 6379, reuse_port: true)
+    # client = server.accept?
   end
 end
 
