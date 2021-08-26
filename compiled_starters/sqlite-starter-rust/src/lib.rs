f1aad22ea0d2dd237dc47f5a1231a8b535f925d6
Odd-Harald Lillestø Myhren <ohmyhren@uia.no>
2021-08-26 19:54:24 +0200

    added implementation

---
+++ compiled_starters/sqlite-starter-rust/src/lib.rs
@@ -0,0 +1,4 @@
+pub mod header;
+pub mod record;
+pub mod schema;
+pub mod varint;
--- compiled_starters/sqlite-starter-rust/src/main.rs
+++ compiled_starters/sqlite-starter-rust/src/main.rs
@@ -1,17 +1,54 @@
-#[allow(unused_imports)]
-use std::env;
-#[allow(unused_imports)]
-use std::fs;
-#[allow(unused_imports)]
-use std::net::TcpListener;
+use anyhow::{bail, Result};
+use sqlite_starter_rust::{
+    header::PageHeader, record::parse_record, schema::Schema, varint::parse_varint,
+};
+use std::convert::TryInto;
+use std::fs::File;
+use std::io::prelude::*;
 
-fn main() {
-    println!("Your code goes here!");
+fn main() -> Result<()> {
+    // Parse arguments
+    let args = std::env::args().collect::<Vec<_>>();
+    match args.len() {
+        0 | 1 => bail!("Missing <database path> and <command>"),
+        2 => bail!("Missing <command>"),
+        _ => {}
+    }
 
-    // Uncomment this block to pass the first stage
-    // let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    // match listener.accept() {
-    //     Ok((_socket, addr)) => println!("accepted new client: {:?}", addr),
-    //     Err(e) => println!("couldn't accept client: {:?}", e),
-    // }
+    // Read database file into database
+    let mut file = File::open(&args[1])?;
+    let mut database = Vec::new();
+    file.read_to_end(&mut database)?;
+
+    // Parse command and act accordingly
+    let command = &args[2];
+    match command.as_str() {
+        ".dbinfo" => {
+            // Parse page header from database
+            let page_header = PageHeader::parse(&database[100..108])?;
+
+            // Obtain all cell pointers
+            let cell_pointers = database[108..]
+                .chunks_exact(2)
+                .take(page_header.number_of_cells.into())
+                .map(|bytes| u16::from_be_bytes(bytes.try_into().unwrap()))
+                .collect::<Vec<_>>();
+
+            // Obtain all records from column 5
+            let schemas = cell_pointers
+                .into_iter()
+                .map(|cell_pointer| {
+                    let stream = &database[cell_pointer as usize..];
+                    let (_, offset) = parse_varint(stream);
+                    let (_rowid, read_bytes) = parse_varint(&stream[offset..]);
+                    parse_record(&stream[offset + read_bytes..], 5)
+                        .map(|record| Schema::parse(record).expect("Invalid record"))
+                })
+                .collect::<Result<Vec<_>>>()?;
+            println!("{}", schemas.len());
+        }
+        _ => bail!("Missing or invalid command passed: {}", command),
+    }
+
+    Ok(())
 }
