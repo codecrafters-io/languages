0868c1b8a2bdb6e550ce8778b89a75cebfc5b68b
Brian Yarr <brian.yarr@deliveroo.co.uk>
2021-09-21 22:01:48 +0100

    compile template

--- compiled_starters/docker-starter-rust/src/main.rs
+++ compiled_starters/docker-starter-rust/src/main.rs
@@ -1,23 +1,25 @@
 use anyhow::Result;
-use std::process::exit;
+use std::process::{exit, Command};
+use std::str;
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 fn main() -> Result<()> {
     println!("Your code goes here!");
 
-    let args: Vec<_> = std::env::args().collect();
-    let command = &args[3];
-    let command_args = &args[4..];
-    let output = std::process::Command::new(command)
-        .args(command_args)
-        .output()?;
-
-    if output.status.success() {
-        let std_out = std::str::from_utf8(&output.stdout)?;
-        println!("{}", std_out)
-    } else {
-        exit(1);
-    }
+    // Uncomment this block to pass the first stage!
+    // let args: Vec<_> = std::env::args().collect();
+    // let command = &args[3];
+    // let command_args = &args[4..];
+    // let output = Command::new(command)
+    //     .args(command_args)
+    //     .output()?;
+    //
+    // if output.status.success() {
+    //     let std_out = str::from_utf8(&output.stdout)?;
+    //     println!("{}", std_out)
+    // } else {
+    //     exit(1);
+    // }
 
     Ok(())
 
