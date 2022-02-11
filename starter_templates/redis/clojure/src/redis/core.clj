4890084dfa7917e1f9e4de4a0ddfaeaef055ebfa
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-02-11 01:28:21 +0000

    add comment to clojure

--- starter_templates/redis/clojure/src/redis/core.clj
+++ starter_templates/redis/clojure/src/redis/core.clj
@@ -31,6 +31,7 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
+  ;; You can use print statements as follows for debugging, they'll be visible when running tests.
   (println "Logs from your program will appear here!")
   ;; Uncomment this block to pass the first stage
   ;; (serve 6379 handler)
