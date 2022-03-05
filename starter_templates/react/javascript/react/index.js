507572d23a85801573a8d04bf5ec6328591d30ad
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-03-05 20:14:23 +0000

    Add your logs will appear here text

--- starter_templates/react/javascript/react/index.js
+++ starter_templates/react/javascript/react/index.js
@@ -1,4 +1,7 @@
 function createElement(type, props, children) {
+    // You can use print statements as follows for debugging, they'll be visible when running tests.
+    console.log('Logs from your program will appear here!');
+
     // Uncomment this to pass the first stage
     // return { type: type };
 }
