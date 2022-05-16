5d508119be8d2ab6cb195d056e7eea66a1cfba54
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-05-17 00:18:51 +0100

    Fix elixir build

--- compiled_starters/redis-starter-elixir/lib/server.ex
+++ compiled_starters/redis-starter-elixir/lib/server.ex
@@ -6,6 +6,7 @@ defmodule Server do
   use Application
 
   def start(_type, _args) do
+    # Uncomment this block to pass the first stage
     Supervisor.start_link([{Task, fn -> Server.listen() end}], strategy: :one_for_one)
   end
 
@@ -16,9 +17,7 @@ defmodule Server do
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     IO.puts("Logs from your program will appear here!")
 
-    # Uncomment this block to pass the first stage
-    #
-    # {:ok, socket} = :gen_tcp.listen(6379, [:binary, active: false, reuseaddr: true])
-    # {:ok, _client} = :gen_tcp.accept(socket)
+    {:ok, socket} = :gen_tcp.listen(6379, [:binary, active: false, reuseaddr: true])
+    {:ok, _client} = :gen_tcp.accept(socket)
   end
 end
