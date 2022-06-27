6b76c6dcfb39708ce46e1833996e5e2142309004
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-06-27 21:15:55 +0100

    fix python starter

--- compiled_starters/redis-starter-python/app/main.py
+++ compiled_starters/redis-starter-python/app/main.py
@@ -1,12 +1,12 @@
+# Uncomment this to pass the first stage
+# import socket
+
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
-    # Uncomment this to pass the first stage
-    #
-    # import socket
-    # s = socket.create_server(("localhost", 6379), reuse_port=True)
-    # s.accept() # wait for client
+    # server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+    # server_socket.accept() # wait for client
 
 
 if __name__ == "__main__":
