f1320ed906fae04c24f2ad808a85cbaba32d7855
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-06-27 21:20:19 +0100

    add empty line after

--- compiled_starters/redis-starter-python/app/main.py
+++ compiled_starters/redis-starter-python/app/main.py
@@ -1,6 +1,7 @@
 # Uncomment this to pass the first stage
 # import socket
 
+
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
