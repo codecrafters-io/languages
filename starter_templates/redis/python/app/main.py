4b2f7403ea85ed7555113644af0d0cfae4df0540
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-06-27 21:17:27 +0100

    fix python starter code naming

--- starter_templates/redis/python/app/main.py
+++ starter_templates/redis/python/app/main.py
@@ -5,6 +5,8 @@ def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
+    # Uncomment this to pass the first stage
+    #
     # server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     # server_socket.accept() # wait for client
 
